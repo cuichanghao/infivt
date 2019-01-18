17f229aef32b553951d5b5d7b377b4512bb38c49
cuichanghao <cuichanghao007@163.com>
2019-01-18 17:44:09 +0900

    change example button color

--- app/src/main/java/cc/cuichanghao/infivt/EntryActivity.kt
+++ app/src/main/java/cc/cuichanghao/infivt/EntryActivity.kt
@@ -10,6 +10,7 @@ class EntryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entry)
+        title = "Infinite Viewpager With TabLayout"
     }
 
     fun onStartNormally(view: View) {
